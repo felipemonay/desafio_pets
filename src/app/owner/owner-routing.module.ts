99f713308efb47035512e6d15888d87a11f8e42c
Felipe Monay <felipemonay@gmail.com>
2020-11-17 19:46:02 -0300

    owner routing module

--- src/app/owner/owner-routing.module.ts
+++ src/app/owner/owner-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OwnerCreateComponent } from './owner-create/owner-create.component';
+import { OwnerListComponent } from './owner-list/owner-list.component';
+import { OwnerUpdateComponent } from './owner-update/owner-update.component';
 
-
-const routes: Routes = [];
+const ownerRoutes: Routes = [
+{ path: '', component: OwnerListComponent },
+{ path: 'create', component: OwnerCreateComponent },
+{ path: 'update', component: OwnerUpdateComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(ownerRoutes)],
   exports: [RouterModule]
 })
 export class OwnerRoutingModule { }
