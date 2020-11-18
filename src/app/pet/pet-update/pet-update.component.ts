004964f2df948173cc8764dc9d6f0639b6c80cdf
Felipe Monay <felipemonay@gmail.com>
2020-11-17 22:07:30 -0300

    pet-update

--- src/app/pet/pet-update/pet-update.component.ts
+++ src/app/pet/pet-update/pet-update.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ErrorHandler, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Pet } from 'src/app/models/pet';
+import { OwnerService } from 'src/app/owner/owner.service';
+import { PetService } from '../pet.service';
 
 @Component({
   selector: 'app-pet-update',
@@ -6,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PetUpdateComponent implements OnInit {
 
-  constructor() { }
+  pet: Pet = new Pet;
+
+  constructor(private ownerService: OwnerService, private toastrService: ToastrService, private activatedRoute: ActivatedRoute, private router: Router, private petService: PetService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.getPet(params['id']);
+    });
+  }
+
+  public getPet(id){
+    this.petService.getPetById(id).subscribe(data => {
+      this.pet = data;
+    }, error => {
+        return new ErrorHandler();
+    });
+  }
+
+  send() {
+    this.petService.updatePet(this.pet).subscribe(success => {
+      this.toastrService.success('', 'Dados Gravados com sucesso!', {timeOut: 1000});
+      return this.router.navigate(['/pet']);
+    }, (err) => {
+      return new ErrorHandler();
+    });
   }
 
 }
