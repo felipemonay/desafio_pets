5aad859cd67d6494bba3428c623e04048f6523a5
Felipe Monay <felipemonay@gmail.com>
2020-11-18 22:59:46 -0300

    nav-bar style

--- src/app/core/layout/nav-bar/nav-bar.component.ts
+++ src/app/core/layout/nav-bar/nav-bar.component.ts
@@ -3,8 +3,7 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  templateUrl: './nav-bar.component.html'
 
   
 })
